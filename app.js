440ae121af59f31d4a1df1399c6f6fbb150e265a
Michael Tan <michaelrstan@gmail.com>
2020-01-31 14:22:04 +1300

    Logo, taskbar and align

--- app.js
+++ app.js
@@ -5,18 +5,41 @@ const bodyParser = require("body-parser");
 const ejs = require("ejs");
 
 const app = express();
-
+const reviewTitle = "Default Title"
+const aboutContent = "Hac habitasse platea dictumst vestibulum rhoncus est pellentesque. Dictumst vestibulum rhoncus est pellentesque elit ullamcorper. Non diam phasellus vestibulum lorem sed. Platea dictumst quisque sagittis purus sit. Egestas sed sed risus pretium quam vulputate dignissim suspendisse. Mauris in aliquam sem fringilla. Semper risus in hendrerit gravida rutrum quisque non tellus orci. Amet massa vitae tortor condimentum lacinia quis vel eros. Enim ut tellus elementum sagittis vitae. Mauris ultrices eros in cursus turpis massa tincidunt dui.";
+const homeStartingContent = "Lacus vel facilisis volutpat est velit egestas dui id ornare. Semper auctor neque vitae tempus quam. Sit amet cursus sit amet dictum sit amet justo. Viverra tellus in hac habitasse. Imperdiet proin fermentum leo vel orci porta. Donec ultrices tincidunt arcu non sodales neque sodales ut. Mattis molestie a iaculis at erat pellentesque adipiscing. Magnis dis parturient montes nascetur ridiculus mus mauris vitae ultricies. Adipiscing elit ut aliquam purus sit amet luctus venenatis lectus. Ultrices vitae auctor eu augue ut lectus arcu bibendum at. Odio euismod lacinia at quis risus sed vulputate odio ut. Cursus mattis molestie a iaculis at erat pellentesque adipiscing.";
 app.set('view engine', 'ejs');
 
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(express.static("public"));
 
+const postSchema = {
+  title: String,
+  content: String
+};
+
+
+
 app.get("/", function(req, res){
-  res.render("home");
+  res.render("home", {startingContent: homeStartingContent,
+    title:reviewTitle
+  });
 });
 
 app.get("/about", function(req, res){
-  res.render("about");
+  res.render("about", {aboutContent: aboutContent});
+});
+
+app.get("/list", function(req, res){
+  res.render("list");
+});
+
+app.get("/submit", function(req, res){
+  res.render("submit");
+});
+
+app.get("/posts/:postID", function(req, res){
+  const requestedPostId = req.params.postId;
 });
 
 app.listen(3000, function(){
