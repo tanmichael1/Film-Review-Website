ba3e85cd2ec10803d82a35bfed4e06320afe40dd
Michael Tan <michaelrstan@gmail.com>
2020-07-10 14:49:18 +1200

    Add .env

---
+++ Portfolio-Website/index.js
@@ -0,0 +1,15 @@
+//const express = require("express");
+
+
+var react= require("react");
+
+import React from "react";
+import ReactDOM from "react-dom";
+import App from "./components/App";
+
+ReactDOM.render(<App />, document.getElementById("root"));
+
+
+app.listen(3000, function() {
+  console.log("Server is running on port 3000");
+});
